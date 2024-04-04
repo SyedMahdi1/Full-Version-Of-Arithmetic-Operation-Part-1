e4412138b9e79b2cbf5b3833a99940ab8ad4d47c
SyedMahdi1 <131530472+SyedMahdi1@users.noreply.github.com>
2024-04-04 02:44:54 -0700

    Enjoy The Codes
    Enjoy The Codes

---
+++ Arithmetic Operations.py
@@ -0,0 +1,39 @@
+# For Addition
+a = 2
+a = a + 2 # Long method
+a += 2# Short Method
+print(a)
+
+# For subtraction
+c = 2
+c = c - 1
+c -= 0.5
+print(c)
+
+# For Multiplication
+
+e = 5
+e *= 5
+print(e)
+
+# For Division
+
+f = 10
+f /= 5
+print(f)
+
+# Now Lets remove the decimal value
+g = 10
+g //= 5
+print(g)
+
+#Lets Get a remainder of a value
+h = 5
+h %= 2
+print(h)
+
+# For Square
+i = 5
+i **= 5
+print(i)
+
